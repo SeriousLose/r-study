e5a46bf2e11f5f41cf4d8b3ca329e2c18de58df6
helongfei <643756132@qq.com>
2021-07-02 17:36:40 +0800

    Revert "<feature> 增加14.15环境"
    This reverts commit f2381c11903db39422d3f1ec3cf2537b344a9a3b.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
 import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -17,7 +17,6 @@ function App() {
         >
           Learn React
         </a>
-        <div>测试</div>
       </header>
     </div>
   );
