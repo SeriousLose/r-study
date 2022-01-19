195568f8baedf61f351213590c59dba3e36aa197
helongfei <643756132@qq.com>
2022-01-19 18:59:36 +0800

    Update DiscussList.jsx

--- src/components/DiscussList/DiscussList.jsx
+++ src/components/DiscussList/DiscussList.jsx
@@ -4,6 +4,10 @@ import { getList } from '../../core/services/httpService';
 function DiscussList(props) {
   const [list, setList] = useState([]);
   const { targetId } = props;
+  function changeText(item) {
+    console.log(item)
+    // return setText("修改后的文本");
+  }
 
   useEffect(() => {
     // setList
@@ -13,12 +17,16 @@ function DiscussList(props) {
       setList(result.list);
     };
     fetchData();
-  },[targetId]);
+  }, [targetId]);
 
   return (
     <div>
       {list.map((item, index) => {
-        return <div key={index}>{item.cont}</div>;
+        return (
+          <div key={index} onClick={() => changeText(item) }>
+            {item.cont}
+          </div>
+        );
       })}
       <p>评论列表</p>
     </div>
